65e0e619fec5743d123202cfa0e36f497bc2527d
strixun <m.seaward@pm.me>
2025-12-28 18:17:19 -0700

    refactor(worker): consolidate type imports in URL shortener worker

--- serverless/url-shortener/worker.ts
+++ serverless/url-shortener/worker.ts
@@ -8,8 +8,7 @@
  */
 
 import { createCORSMiddleware } from '@strixun/api-framework/enhanced';
-import { initializeServiceTypes } from '../shared/types.js';
-import type { ExecutionContext } from '../shared/types.js';
+import { initializeServiceTypes, ExecutionContext } from '../shared/types.js';
 import { createRouter } from './router/routes.js';
 
 /**
